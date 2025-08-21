5fd44eaf746e5692147582313a13d004865916e3
Hoàng Văn Hải <hoanggvanhaii0705@gmail.com>
2025-08-21 20:46:41 +0700

    Delete route test in the Server

--- server.ts
+++ server.ts
@@ -3,21 +3,6 @@ import dotenv from 'dotenv';
 dotenv.config();
 const app = express();
 app.use(express.json());
-import { connectionDB } from './src/config/database';
-import { Request, Response } from 'express';
-app.get('/test-db', async (req: Request, res: Response) => {
-    try {
-        const pool = await connectionDB();
-        res.json({
-            message: 'Kết nối DB thành công!',
-        });
-    } catch (err: any) {
-        res.status(500).json({
-            message: 'Lỗi kết nối DB',
-            error: err.message
-        });
-    }
-});
 app.listen(process.env.PORT, () => {
     console.log(`Server is running in  http://localhost:${process.env.PORT}`);
 })
