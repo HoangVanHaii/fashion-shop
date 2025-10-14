74ce9917e2eb294d246035550c2cf5a9ea577c71
nguyenhaidangg <4901104031@student.hcmue.edu.vn>
2025-10-14 16:27:22 +0700

    API Review new

--- src/controllers/review.ts
+++ src/controllers/review.ts
@@ -5,11 +5,11 @@ import { AppError } from '../utils/appError';
 
 export const createReview = async (req: Request, res: Response, next: NextFunction) => {
     try {
-        const { product_id, rating, comment } = req.body;
+        const { order_item_id, rating, comment } = req.body;
         const user_id = req.user!.id;
         const reviewImages = (req.files as Express.Multer.File[]) || [];
         const pathImages: ReviewImage[] = reviewImages.map(file =>({ image_url: `/uploads/reviews/${file.filename}`}));
-        const review: Review = { product_id, user_id, rating, comment, review_images: pathImages };
+        const review: Review = { order_item_id, user_id, rating, comment, review_images: pathImages };
         await reviewService.createReview(review);
         res.status(201).json({
             success: true,
@@ -29,13 +29,15 @@ export const getReviewsByProductId = async (req: Request, res: Response, next: N
             data: reviews
         });
     } catch (err) {
+        console.log(err);
         next(err);
     }
 }
 export const removeReviewById = async (req: Request, res: Response, next: NextFunction) => {
     try {
         const review_id = parseInt(req.params.review_id);
-        await reviewService.removeReviewById(review_id);
+        const user_id = req.user!.id;
+        await reviewService.removeReviewById(review_id,user_id);
         return res.status(200).json({
             success: true,
             message: "Review deleted successfully"
@@ -46,8 +48,8 @@ export const removeReviewById = async (req: Request, res: Response, next: NextFu
 }
 export const getReviewById = async (req: Request, res: Response, next: NextFunction) => {
     try {
-        const id = req.params.review_id;
-        const review = await reviewService.getReviewById(parseInt(id));
+        const id = parseInt(req.params.review_id);
+        const review = await reviewService.getReviewById(id);
         if (!review) {
             throw new AppError("Review not found", 404);
         }
@@ -62,3 +64,35 @@ export const getReviewById = async (req: Request, res: Response, next: NextFunct
     }
 }
 
+export const updateReview = async (req: Request, res: Response, next: NextFunction): Promise<void> =>{
+    try {
+        const review_id = parseInt(req.params.review_id);
+        const user_id = req.user!.id;
+        const existingReview = await reviewService.getReviewById(review_id);
+        if (!existingReview) {
+            throw new AppError("Review not found", 404);
+        }
+        //Lấy ảnh mới
+        const reviewImages = (req.files as Express.Multer.File[]) || [];
+        const pathImages: ReviewImage[] = reviewImages.map(file =>({ image_url: `/uploads/reviews/${file.filename}`}));
+       
+        const review: Review = {
+            id:existingReview.id,
+            order_item_id: existingReview.order_item_id,
+            user_id: req.user!.id,
+            rating: req.body.rating ?? existingReview.rating,
+            comment: req.body.comment ?? existingReview.comment,
+            created_at: existingReview.created_at,
+            review_images: pathImages.length > 0 ? pathImages : existingReview.review_images
+        };
+        await reviewService.updateReview(review);
+
+        res.status(200).json({
+            success: true,
+            message: "Review updated successfully"
+        });
+    } catch (error) {
+        console.log(error)
+        next(error);
+    }
+}
--- src/services/review.ts
+++ src/services/review.ts
@@ -9,13 +9,13 @@ export const createReview = async (review: Review): Promise<void> => {
         await transaction.begin();
         const insertReviewResult = await transaction
             .request()
-            .input("product_id", review.product_id)
+            .input("order_item_id", review.order_item_id)
             .input("user_id", review.user_id)
             .input("rating", review.rating)
             .input("comment", review.comment)
-            .query(`INSERT INTO reviews (product_id, user_id, rating, comment)
+            .query(`INSERT INTO reviews (order_item_id, user_id, rating, comment, created_at)
                     OUTPUT INSERTED.id
-                    VALUES (@product_id, @user_id, @rating, @comment)`);
+                    VALUES (@order_item_id, @user_id, @rating, @comment, GETDATE())`);
         
         const reviewId = insertReviewResult.recordset[0].id;
 
@@ -41,11 +41,23 @@ export const getReviewsByProductId = async (product_id: number): Promise<ReviewO
         const result = await pool
             .request()
             .input("product_id", product_id)
-            .query(`SELECT r.id, r.product_id, r.user_id, r.rating, r.comment, r.created_at,
-                           ri.id AS review_image_id, ri.image_url
-                    FROM reviews r
-                    LEFT JOIN review_images ri ON r.id = ri.review_id
-                    WHERE r.product_id = @product_id`);
+            .query(`
+                SELECT r.id AS review_id,
+                       r.order_item_id,
+                       r.user_id,
+                       r.rating,
+                       r.comment,
+                       r.created_at,
+                       ri.id AS review_image_id,
+                       ri.image_url
+                FROM reviews r
+                INNER JOIN order_items oi ON r.order_item_id = oi.id
+                INNER JOIN product_sizes ps ON oi.size_id = ps.id
+                INNER JOIN product_colors pc ON ps.color_id = pc.id
+                LEFT JOIN review_images ri ON r.id = ri.review_id
+                WHERE pc.product_id = @product_id
+                
+            `);
         const reviewsMap: { [key: number]: Review } = {};
         const total_reviews = result.recordset.length;
         const average_rating = result.recordset.reduce((acc, row) => acc + row.rating, 0) / total_reviews;
@@ -53,7 +65,7 @@ export const getReviewsByProductId = async (product_id: number): Promise<ReviewO
             if (!reviewsMap[row.id]) {
                 reviewsMap[row.id] = {
                     id: row.id,
-                    product_id: row.product_id,
+                    order_item_id: row.order_item_id,
                     user_id: row.user_id,
                     rating: row.rating,
                     comment: row.comment,
@@ -77,27 +89,41 @@ export const getReviewsByProductId = async (product_id: number): Promise<ReviewO
         throw new AppError("Failed to get reviews", 500, false);
     }
 }
-export const removeReviewById = async (review_id: number): Promise<void> => {
+export const removeReviewById = async (review_id: number, user_id: number): Promise<void> => {
     try {
         const pool = await connectionDB();
-        await pool
+        const result = await pool
             .request()
             .input("review_id", review_id)
-            .query(`DELETE FROM review_images WHERE review_id = @review_id;
-                    DELETE FROM reviews WHERE id = @review_id`);
-        
+            .input("user_id", user_id)
+            .query(`
+                DELETE FROM review_images 
+                WHERE review_id IN (
+                    SELECT id FROM reviews WHERE id = @review_id AND user_id = @user_id
+                );
+
+                DELETE FROM reviews
+                WHERE id = @review_id AND user_id = @user_id;
+            `);
+
+        if (result.rowsAffected[1] === 0) {
+            throw new AppError("You are not allowed to delete this review", 403, true);
+        }
+
     } catch (err) {
         console.error(err);
+        if (err instanceof AppError) throw err;
         throw new AppError("Failed to delete review", 500, false);
     }
 }
+
 export const getReviewById = async (review_id: number): Promise<Review | null> => {
     try {
         const pool = await connectionDB();
         const result = await pool
             .request()
             .input("review_id", review_id)
-            .query(`SELECT r.id, r.product_id, r.user_id, r.rating, r.comment, r.created_at,
+            .query(`SELECT r.id, r.order_item_id, r.user_id, r.rating, r.comment, r.created_at,
                            ri.id AS review_image_id, ri.image_url
                     FROM reviews r
                     LEFT JOIN review_images ri ON r.id = ri.review_id
@@ -109,7 +135,7 @@ export const getReviewById = async (review_id: number): Promise<Review | null> =
 
         const review: Review = {
             id: result.recordset[0].id,
-            product_id: result.recordset[0].product_id,
+            order_item_id: result.recordset[0].order_item_id,
             user_id: result.recordset[0].user_id,
             rating: result.recordset[0].rating,
             comment: result.recordset[0].comment,
@@ -133,3 +159,54 @@ export const getReviewById = async (review_id: number): Promise<Review | null> =
         throw new AppError("Failed to get review", 500, false);
     }
 }
+
+export const updateReview = async (review: Review): Promise<void> => {
+    const pool = await connectionDB();
+    const transaction = await pool.transaction();
+    try {
+        await transaction.begin();
+
+        const updateResult = await transaction
+            .request()
+            .input("review_id", review.id)
+            .input("user_id", review.user_id)
+            .input("rating", review.rating)
+            .input("comment", review.comment)
+            .input("created_at", review.created_at)
+            .query(`
+                UPDATE reviews
+                SET 
+                    rating = @rating,
+                    comment = @comment,
+                    created_at = @created_at
+                WHERE id = @review_id AND user_id = @user_id
+            `);
+
+        if (updateResult.rowsAffected[0] === 0) {
+            
+            throw new AppError("You are not allowed to update this review", 403);
+        }
+
+        if (review.review_images && review.review_images.length > 0) {
+            await transaction
+                .request()
+                .input("review_id", review.id)
+                .query(`DELETE FROM review_images WHERE review_id = @review_id`);
+
+            for (const image of review.review_images) {
+                await transaction
+                    .request()
+                    .input("review_id", review.id)
+                    .input("image_url", image.image_url)
+                    .query(`INSERT INTO review_images (review_id, image_url)
+                            VALUES (@review_id, @image_url)`);
+            }
+        }
+
+        await transaction.commit();
+    } catch (error) {
+        await transaction.rollback();
+        console.error(error);
+        throw new AppError("Failed to update review", 500, false);
+    }
+};
