35d74771541eb69baef799f0e35a65ac8ae653cc
Hoàng Văn Hải <hoanggvanhaii0705@gmail.com>
2025-10-16 00:29:03 +0700

    refactor folder architechture

--- Be/src/routers/product.ts
+++ Be/src/routers/product.ts
@@ -17,6 +17,10 @@ router.get(
     productController.getProductByShop
 );
 router.get(
+    '/searchByCategoryGender',
+    productController.getProductByCategoryGender
+)
+router.get(
     "/searchByCategory",
     productController.getProductsByCategory
 );
